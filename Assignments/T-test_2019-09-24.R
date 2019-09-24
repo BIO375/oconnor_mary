d48d2599b5118cbec3f984f88cf10c73afc2bad8
maryoconnor2021 <mcatherineo317@gmail.com>
2019-09-24 17:34:11 -0400

    Lab work for 9-24-2109

---
+++ Assignments/T-test_2019-09-24.R
@@ -0,0 +1,30 @@
+library("tidyverse")
+tidyverse_update()
+install.packages ("tidyr")
+birthrate<-read_csv("datasets/demos/birthrate_diff.csv")
+ggplot(birthrate) +
+  geom_histogram(aes(diff), binwidth = 1)
+ggplot(birthrate)+
+  geom_boxplot(aes(y=diff),notch=FALSE, varwidth = TRUE)
+summ_diff<-birthrate%>%
+  summarise(mean_diff=mean(diff),
+            median_diff=median(diff),
+            IQR_diff=IQR(diff),
+            sd_diff=sd(diff),
+            var_diff=var(diff))
+data01<-read_csv("datasets/abd/chapter12/chap12e3HornedLizards.csv")
+data01<-data01%>%slice(-105)
+ggplot(data01)+
+  geom_histogram(aes(squamosalHornLength), binwidth= 5)+facet_wrap(~"living")
+ggplot(data01)+
+  geom_histogram(aes(squamosalHornLength), binwidth= 5)+ facet_wrap(~"killed")
+ggplot(data01)+
+  geom_boxplot(aes(x=Survival,y=squamosalHornLength),notch=FALSE, varwidth = TRUE)
+names(data01)
+summ_squamosalHornLength<-data01%>%
+  group_by(Survival)%>%
+  summarise(mean_squamosalHornLength=mean(squamosalHornLength),
+            median_squamosalHornLength=median(squamosalHornLength),
+            IQR_squamosalHornLength=IQR(squamosalHornLength),
+            sd_squamosalHornLength=sd(squamosalHornLength),
+            var_squamosalHornLength=var(squamosalHornLength))
